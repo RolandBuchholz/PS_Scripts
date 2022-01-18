5c8d361e823e822d26d5f71238a2d9985bbfaed2
RolandBuchholz <roland.buchholz@gmx.de>
2022-01-18 13:54:08 +0100

    ReadOnlyParm  integrated

--- GetVaultFile.ps1
+++ GetVaultFile.ps1
@@ -6,25 +6,28 @@
      File Name : GetVaultFile.ps1
      Author : Buchholz Roland – roland.buchholz@berchtenbreiter-gmbh.de
 .VERSION
-     Version Version 0.3 – Testversion Errorcode handling hizugefügt
+     Version Version 0.8 – ReadOnly und VDF Login
 .EXAMPLE
-     Beispiel wie das Script aufgerufen wird > GetVaultFile.ps1 -Auftragsnummer „8951234“
+     Beispiel wie das Script aufgerufen wird > GetVaultFile.ps1 8951234 $true
+                                                        (Auftragsnummer)(ReadOnly)
 .INPUTTYPE
-     Auftragsnummer 
+     [String]Auftragsnummer
+     [bool]ReadOnly  
 .RETURNVALUE
-     Void
+     $errCode
 .COMPONENT
      Vault Server
 #>
-
-       
-Param(            
-[String]$Auftragsnummer           
-)  
-
+      
+Param(
+    [Parameter(Mandatory = $true)]          
+    [String]$Auftragsnummer,
+    [bool]$ReadOnly = $false       
+)
+   
 # Vault Login
 
-try{
+try {
     Import-Module powerVault
     Initialize-VDF
     #Create-LogRepository
@@ -36,78 +39,74 @@ try{
 
     Open-VaultConnection -Password $vaultpW -Server 192.168.0.1:8080 -User $vaultUser -Vault vault
 }
-catch
-{
-    $errCode = "2" #Login fehlgeschlagen
-    $Host.SetShouldExit($errCode -as [int])
+catch {
+    $errCode = 2 #Login fehlgeschlagen
+    $Host.SetShouldExit([int]$errCode)
     exit
 }
 
-    #$Auftragsnummer = "1001042"
-try
-{
+try {
     #Dateinamen der benötigten Dateien
-    $downloadFiles =@()
-    $downloadFiles +=$Auftragsnummer+"-AutoDeskTransfer.xml"
-    $downloadFiles +=$Auftragsnummer+".html"
-    $downloadFiles +=$Auftragsnummer+".aus"
-    $downloadFiles +=$Auftragsnummer+".dat"
-    $downloadFiles +=$Auftragsnummer+".LILO"
+    $downloadFiles = @()
+    $downloadFiles += $Auftragsnummer + "-AutoDeskTransfer.xml"
+    $downloadFiles += $Auftragsnummer + ".html"
+    $downloadFiles += $Auftragsnummer + ".aus"
+    $downloadFiles += $Auftragsnummer + ".dat"
+    $downloadFiles += $Auftragsnummer + ".LILO"
 
     #Dateien im Vault suchen und den Arbeitsbereich ermitteln 
-    $vaultFiles=@()
-    for ($i=0; $i -le $downloadFiles.Count-1; $i++){
+    $vaultFiles = @()
+    for ($i = 0; $i -le $downloadFiles.Count - 1; $i++) {
 
-        $vaultFiles+= Get-VaultFiles -FileName $downloadFiles[$i]
+        $vaultFiles += Get-VaultFiles -FileName $downloadFiles[$i]
     }
 
 
-    if ($vaultFiles.count -gt 0){
-        $WorkFolderPath = $vaultFiles[0].Path.TrimStart("$")-replace "/AUFTRÄGE","C:/Work/AUFTRÄGE"
+    if ($vaultFiles.count -gt 0) {
+        $WorkFolderPath = $vaultFiles[0].Path.TrimStart("$") -replace "/AUFTRÄGE", "C:/Work/AUFTRÄGE"
     }
     #Verzeichnissstruktur anlegen
-        if (!(Test-Path $WorkFolderPath"/Berechnungen")) {New-Item -Path $WorkFolderPath"/Berechnungen" -ItemType Directory}
-        if (!(Test-Path $WorkFolderPath"/Berechnungen/PDF")) {New-Item -Path $WorkFolderPath"/Berechnungen/PDF" -ItemType Directory}
-        if (!(Test-Path $WorkFolderPath"/Bestellungen")) {New-Item -Path $WorkFolderPath"/Bestellungen" -ItemType Directory}
-        if (!(Test-Path $WorkFolderPath"/Bgr00")) {New-Item -Path $WorkFolderPath"/Bgr00" -ItemType Directory}
-        if (!(Test-Path $WorkFolderPath"/Bgr00/CAD-CFP")) {New-Item -Path $WorkFolderPath"/Bgr00/CAD-CFP" -ItemType Directory}
-        if (!(Test-Path $WorkFolderPath"/Fotos")) {New-Item -Path $WorkFolderPath"/Fotos" -ItemType Directory}
-        if (!(Test-Path $WorkFolderPath"/SV")) {New-Item -Path $WorkFolderPath"/SV" -ItemType Directory}
-        if (!(Test-Path $WorkFolderPath"/Montage-TÜV-Dokumentation")) {New-Item -Path $WorkFolderPath"/Montage-TÜV-Dokumentation" -ItemType Directory}
-        if (!(Test-Path $WorkFolderPath"/Montage-TÜV-Dokumentation/TÜV")) {New-Item -Path $WorkFolderPath"/Montage-TÜV-Dokumentation/TÜV" -ItemType Directory}
-        if (!(Test-Path $WorkFolderPath"/Montage-TÜV-Dokumentation/TÜV/Zertifikate")) {New-Item -Path $WorkFolderPath"/Montage-TÜV-Dokumentation/TÜV/Zertifikate" -ItemType Directory}
+    if (!(Test-Path $WorkFolderPath"/Berechnungen")) { New-Item -Path $WorkFolderPath"/Berechnungen" -ItemType Directory }
+    if (!(Test-Path $WorkFolderPath"/Berechnungen/PDF")) { New-Item -Path $WorkFolderPath"/Berechnungen/PDF" -ItemType Directory }
+    if (!(Test-Path $WorkFolderPath"/Bestellungen")) { New-Item -Path $WorkFolderPath"/Bestellungen" -ItemType Directory }
+    if (!(Test-Path $WorkFolderPath"/Bgr00")) { New-Item -Path $WorkFolderPath"/Bgr00" -ItemType Directory }
+    if (!(Test-Path $WorkFolderPath"/Bgr00/CAD-CFP")) { New-Item -Path $WorkFolderPath"/Bgr00/CAD-CFP" -ItemType Directory }
+    if (!(Test-Path $WorkFolderPath"/Fotos")) { New-Item -Path $WorkFolderPath"/Fotos" -ItemType Directory }
+    if (!(Test-Path $WorkFolderPath"/SV")) { New-Item -Path $WorkFolderPath"/SV" -ItemType Directory }
+    if (!(Test-Path $WorkFolderPath"/Montage-TÜV-Dokumentation")) { New-Item -Path $WorkFolderPath"/Montage-TÜV-Dokumentation" -ItemType Directory }
+    if (!(Test-Path $WorkFolderPath"/Montage-TÜV-Dokumentation/TÜV")) { New-Item -Path $WorkFolderPath"/Montage-TÜV-Dokumentation/TÜV" -ItemType Directory }
+    if (!(Test-Path $WorkFolderPath"/Montage-TÜV-Dokumentation/TÜV/Zertifikate")) { New-Item -Path $WorkFolderPath"/Montage-TÜV-Dokumentation/TÜV/Zertifikate" -ItemType Directory }
 
     #Dateien werden ausgechecked
     $downloadTicket = New-Object Autodesk.Connectivity.WebServices.ByteArray
 
-    for ($i=0; $i -le $vaultFiles.Count-1; $i++){
+    for ($i = 0; $i -le $vaultFiles.Count - 1; $i++) {
 
-        If ($vaultFiles[$i].IsCheckedOut){
+        If ($vaultFiles[$i].IsCheckedOut) {
             Write-Host "Datei"$vaultFiles[$i]._Name"ist bereits ausgechecked!"  
         }
-        Else{
-            $checkedOutFile = $vault.DocumentService.CheckoutFile($vaultFiles[$i].Id,"Master",$env:COMPUTERNAME,$WorkFolderPath,"Für automatsische Bearbeitung abgerufen.",[ref]$downloadTicket)
+        Else {
+            $checkedOutFile = $vault.DocumentService.CheckoutFile($vaultFiles[$i].Id, "Master", $env:COMPUTERNAME, $WorkFolderPath, "Für automatsische Bearbeitung abgerufen.", [ref]$downloadTicket)
             Write-Host "Datei"$vaultFiles[$i]._Name"wurde ausgechecked!"   
         }
     }
 
     #Dateien werden in den Arbeitsbereich geladen
-    if ($vaultFiles.count -gt 0)
-    {
-        for ($i=0; $i -le $vaultFiles.Count-1; $i++){
+    if ($vaultFiles.count -gt 0) {
+        for ($i = 0; $i -le $vaultFiles.Count - 1; $i++) {
             $downloadFile = Save-VaultFile -File $vaultFiles[$i].'Full Path'
-            $downloadFullFileName=$vaultFiles[$i]._FullPath.TrimStart("$")-replace "/AUFTRÄGE","C:/Work/AUFTRÄGE"
+            $downloadFullFileName = $vaultFiles[$i]._FullPath.TrimStart("$") -replace "/AUFTRÄGE", "C:/Work/AUFTRÄGE"
             Set-ItemProperty $downloadFullFileName -Name IsReadOnly -Value $false
             Write-Host "Datei"$vaultFiles[$i]._Name"wurde heruntergeladen!"
-            }
+        }
     }
-    Else{
+    Else {
         Write-Host "Auftrag $Auftragsnummer nicht gefunden! Prüfen Sie ob im Vault bereits ein Auftrag angelegt worden ist!!"
         $vault.Dispose() #Vault Connection schließen
 
-        $errCode = "1" # Datei download ist fehlgeschlagen
+        $errCode = 1 # Datei download ist fehlgeschlagen
 
-        $Host.SetShouldExit($errCode -as [int])
+        $Host.SetShouldExit([int]$errCode)
         exit
     }
 
@@ -115,18 +114,17 @@ try
 
     $vault.Dispose() #Vault Connection schließen
 
-    $errCode = "0"
+    $errCode = 0
 
-    $Host.SetShouldExit($errCode -as [int])
+    $Host.SetShouldExit([int]$errCode)
     exit
 }
-catch
-{
+catch {
     $vault.Dispose() #Vault Connection schließen
 
-    $errCode = "1" # Datei download ist fehlgeschlagen
+    $errCode = 1 # Datei download ist fehlgeschlagen
 
-    $Host.SetShouldExit($errCode -as [int])
+    $Host.SetShouldExit([int]$errCode)
     exit
 
 }
---
+++ VDFLoginTest.ps1
@@ -0,0 +1,132 @@
+Add-Type -path "C:\Program Files\Autodesk\Vault Client 2021\Explorer\Autodesk.DataManagement.Client.Framework.Vault.Forms.dll"
+Add-Type -path "C:\Program Files\Autodesk\Vault Client 2021\Explorer\Autodesk.DataManagement.Client.Framework.Vault.dll"
+
+[System.Reflection.Assembly]::LoadFrom($Env:ProgramData + "\Autodesk\Vault 2022\Extensions\DataStandard\Vault.Custom\addinVault\VdsSampleUtilities.dll")
+[System.Reflection.Assembly]::LoadFrom($Env:ProgramData + "\Autodesk\Vault 2021\Extensions\DataStandard\Vault.Custom\addinVault\QuickstartUtilityLibrary.dll")
+[System.Reflection.Assembly]::LoadFrom("C:\Work\Administration\Standardeinstellungen\Inventor\Ilogic\Bin\iLogicAdd\QuickstartiLogicLibrary.dll")
+[System.Reflection.Assembly]::LoadFrom("C:\Work\Administration\Standardeinstellungen\Inventor\Ilogic\Bin\iLogicAdd\QuickstartiLogicVltInvSrvLibrary.dll")
+
+$ReadOnly = $false
+$ReadOnly = $true     
+
+if ($ReadOnly) {
+    try {
+        $serverName = "192.168.0.1:8080"
+        $vaultName = "vault"
+        $vaultUser = "BE-Automation"
+        $vaultPw = "BE-Automation"
+        $authenticationFlags = [Autodesk.DataManagement.Client.Framework.Vault.Currency.Connections.AuthenticationFlags]::ReadOnly
+        $vdfResults = [Autodesk.DataManagement.Client.Framework.Vault.Library]::ConnectionManager.LogIn($serverName, $vaultName, $vaultUser, $vaultPw, $authenticationFlags, $null)
+        
+        if ($vdfResults.Success) {
+            $connection = $vdfResults.Connection;
+        }
+    }
+    catch {
+        $errCode = 2 #Login fehlgeschlagen
+        $Host.SetShouldExit([int]$errCode)
+        exit
+    }
+}
+else {
+    try {
+        $settings = New-Object Autodesk.DataManagement.Client.Framework.Vault.Forms.Settings.LoginSettings
+        $settings.ServerName = "192.168.0.1:8080"
+        $settings.VaultName = "vault"
+        $settings.AutoLoginMode = 3
+        $connection = [Autodesk.DataManagement.Client.Framework.Vault.Forms.Library]::Login($settings)
+
+        if ($null -eq $connection) {
+            $settings.AutoLoginMode = 1
+            $connection = [Autodesk.DataManagement.Client.Framework.Vault.Forms.Library]::Login($settings)
+        }
+    }
+    catch {
+        $errCode = 2 #Login fehlgeschlagen
+        $Host.SetShouldExit([int]$errCode)
+        exit
+    }   
+}
+
+
+function FindFile($fileName) {
+    $filePropDefs = $vault.PropertyService.GetPropertyDefinitionsByEntityClassId("FILE")
+    $fileNamePropDef = $filePropDefs | Where-Object { $_.SysName -eq "Name" }
+    $srchCond = New-Object 'Autodesk.Connectivity.WebServices.SrchCond'
+    $srchCond.PropDefId = $fileNamePropDef.Id
+    $srchCond.PropTyp = "SingleProperty"
+    $srchCond.SrchOper = 3 #is equal
+    $srchCond.SrchRule = "Must"
+    $srchCond.SrchTxt = $fileName
+
+    $bookmark = ""
+    $status = $null
+    $totalResults = @()
+    while ($null -eq $status -or $totalResults.Count -lt $status.TotalHits) {
+        $results = $vault.DocumentService.FindFilesBySearchConditions(@($srchCond), $null, $null, $false, $true, [ref]$bookmark, [ref]$status)
+        if ($null -ne $results) {
+            $totalResults += $results
+        }
+        else { break }
+    }
+    return $totalResults;
+}
+
+
+
+
+$vault = $connection.WebServiceManager
+
+
+
+$downloadFullFileName = FindFile("1001042-AutoDeskTransfer.xml")
+
+
+$user = $connection.UserName
+
+# $ILogicLibrary = New-Object QuickstartiLogicLibrary.QuickstartiLogicLib
+
+
+# $ILogicLibrarySrv = New-Object QuickstartiLogicVltInvSrvLibrary.iLogicVltInvSrvLibrary
+
+# $InvHelpers = New-Object QuickstartUtilityLibrary.InvHelpers
+# $AcadHelpers = New-Object QuickstartUtilityLibrary.AcadHelpers
+$VltHelpers = New-Object QuickstartUtilityLibrary.VltHelpers
+$VltHelpers2 = New-Object VdsSampleUtilities.VltHelpers
+
+
+$VltHelpers2
+
+
+$Path = "$/Administration/Vault.ico"
+$Test = $VltHelpers.mGetFileByFullFileName($connection, $Path)
+
+
+# $Test = $ILogicLibrarySrv.GetFileByFullFilePath($Path)
+# $Test
+
+
+Add-Type -AssemblyName System.Windows.Forms
+Add-Type -AssemblyName System.Drawing
+$window = New-Object System.Windows.Forms.Form
+$window.Width = 400
+$window.Height = 100
+ 
+$Label = New-Object System.Windows.Forms.Label
+$Label.Location = New-Object System.Drawing.Size(10, 10)
+$Label.Text = $user
+$Label.AutoSize = $True
+
+$Label2 = New-Object System.Windows.Forms.Label
+$Label2.Location = New-Object System.Drawing.Size(10, 30)
+$Label2.Text = $downloadFullFileName.Name
+$Label2.AutoSize = $True
+
+$window.Controls.Add($Label)
+$window.Controls.Add($Label2)
+[void]$window.ShowDialog()
+
+$vault.Dispose()
+$logOff = [Autodesk.DataManagement.Client.Framework.Vault.Library]::ConnectionManager.LogOut($connection)
+
+
